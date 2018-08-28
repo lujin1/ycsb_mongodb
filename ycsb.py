8980f5ae5dde7950b738f85e6e7d0f27865d3794
xiaojin525 <1395856829@qq.com>
2018-08-28 13:32:19 +0800

    更新 ycsb.py

--- ycsb.py
+++ ycsb.py
@@ -6,44 +6,36 @@ from pymongo import MongoClient
 import ConfigParser
 
 def drop_collection(mongodb_url):
-	print ("drop ycsb.usertable")
-	client = MongoClient(mongodb_url)
-	db = client.ycsb
-	collection = db.usertable
-	result = collection.drop()
-	print (result)
-	
-def run(mongodb_url, recordcount, threads, work, insertproportion):
-	result = commands.getoutput('sh ycsb.sh %s %s %s %s %d' %(mongodb_url, recordcount, threads, work, insertproportion))
-	print (result)
+        print ("drop ycsb.usertable")
+        client = MongoClient(mongodb_url)
+        db = client.ycsb
+        collection = db.usertable
+        result = collection.drop()
+        print (result)
+
+def run(mongodb_url, recordcount, threads, work, insertproportion, ycsb_dir):
+        result = commands.getoutput('sh ycsb.sh %s %s %s %s %d %s' %(mongodb_url, recordcount, threads, work, insertproportion, ycsb_dir))
+        print (result)
 
 def get_model(work):
-	with open(work,'r') as f:
-		for line in f.readlines():
-			if "insertproportion" in line:
-				insertproportion = int(line.split('=')[1])
-				return insertproportion
+        with open(work,'r') as f:
+                for line in f.readlines():
+                        if "insertproportion" in line:
+                                insertproportion = int(line.split('=')[1])
+                                return insertproportion
 
 if __name__=="__main__":
-	conf = ConfigParser.ConfigParser() 
-	conf.read("config.ini")
-	mongodb_url = conf.get("mongodb","mongodb_url")
-	work = conf.get("ycsb","work")
-	recordcount_list = conf.get("ycsb","recordcount_list")
-	threads_list = conf.get("ycsb","threads_list")
-	insertproportion = get_model(work)
-	for recordcount in recordcount_list:
-		for threads in threads_list:
-			drop_collection(mongodb_url)
-			start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-			run(mongodb_url, recordcount, threads, work, insertproportion)
-			end_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-			print ("时间间隔：%s--%s"%(start_time, end_time))
-			try:
-				file = "result/y_" + recordcount + "_" + threads
-				ff = open(file,'a')
-				ff.write("start_time:%s\n" %start_time)
-				ff.write("start_time:%s\n" %start_time)
-				ff.close()
-			time.sleep(600)
-	print (commands.getoutput('cat lujin.txt'))
+        conf = ConfigParser.ConfigParser()
+        conf.read("config.ini")
+        mongodb_url = conf.get("mongodb","mongodb_url")
+        work = conf.get("ycsb","work")
+        recordcount_list = conf.get("ycsb","recordcount_list").split(',')
+        threads_list = conf.get("ycsb","threads_list").split(',')
+        insertproportion = get_model(work)
+        ycsb_dir = conf.get("ycsb","ycsb_dir")
+        for recordcount in recordcount_list:
+                for threads in threads_list:
+                        drop_collection(mongodb_url)
+                        run(mongodb_url, recordcount, threads, work, insertproportion, ycsb_dir)
+                        time.sleep(600)
+        print (commands.getoutput('cat lujin.txt'))
